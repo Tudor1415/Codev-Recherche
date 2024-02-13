b8a09b68c9442bbd586b4c74849e0754dcd64a32
Tudor1415 <tudoropran14@gmail.com>
2024-02-13 18:59:39 +0100

    Simulation with no bugs, untested

--- TODOS/14-02-24/Gibbs Sampling using Arules/Simulation.r
+++ TODOS/14-02-24/Gibbs Sampling using Arules/Simulation.r
@@ -29,6 +29,61 @@ inspect(transactions)
 
 # Assuming 'transactions' is your transaction data
 rules <- apriori(transactions , 
-                 parameter = list(supp = 0.1, conf = 0.9, target = "rules"),
+                 parameter = list(supp = 0, conf = 0.8, target = "rules"),
                  appearance = list(rhs = c("J_C", "J_NC"), lhs = c("J_1", "J_2", "J_3")))
 inspect(rules)
+
+# Define g_function as the product of support and confidence
+g_function <- function(J, transactions) {
+  J_transaction <- as(as.matrix(J), "transactions")
+
+  Jant_transaction <- as(as.matrix(J[, -c(ncol(J) - 1, ncol(J))]), "transactions")
+
+  # Calculate support
+  support_J <- support(J_transaction, transactions)
+  support_Jant <- support(Jant_transaction, transactions)
+
+  # Calculate confidence
+  confidence <- support_J / support_Jant
+
+  return(support_J * confidence)
+}
+
+# Define Gibbs sampling algorithm
+gibbs_sampling <- function(transactions, iterations, csi, g_function) {
+  # Initialize a binary vector J
+  J <- data.frame(
+   J_1 = sample(0:1, size = 1, replace = TRUE),
+   J_2 = sample(0:1, size = 1, replace = TRUE),
+   J_3 = sample(0:1, size = 1, replace = TRUE),
+   J_C = sample(0:1, size = 1, replace = TRUE),
+   J_NC = sample(0:1, size = 1, replace = TRUE)
+  )
+  
+  # List to store the sample of binary vectors 
+  sample_list <- list()
+  
+  # Perform Gibbs sampling for the specified number of iterations
+  for (iteration in 1:iterations) {
+    # Iterate over each element in the binary vector J
+    for (i in 1:(no_items - 1)) {
+      # Append the current state of the binary vector to the sample
+      sample_list[[length(sample_list) + 1]] <- J
+      
+      # Toggle the i-th element and calculate the probability
+      J[i] <- 1
+      probability <- exp(csi * g_function(J, transactions))
+      J[i] <- 0
+      probability <- probability / (exp(csi * g_function(J, transactions)) + probability)
+      
+      # Generate a random sample from a Bernoulli distribution with the calculated probability
+      J[i] <- rbinom(1, 1, probability)
+    }
+  }
+  
+  # Return the sample of binary vectors 
+  return(sample_list)
+}
+
+# Run Gibbs sampling
+result <- gibbs_sampling(transactions, 10, 3, g_function)
